d1b31067cff32125693533976ef62175bd7f6554
jihm <ajihmallekine@gmail.com>
2026-08-31 11:00:01 +0800

    chore: remove obsolete workspace route placeholders

--- src/app/activity-designs/[id]/page.tsx
+++ src/app/activity-designs/[id]/page.tsx
@@ -38,7 +38,7 @@ export default async function ActivityDesignDetailRoute({
         name: session.user.name ?? session.user.username ?? "Municipal staff",
         username: session.user.username ?? "staff account",
       }}
-      activePath="/activity-designs"
+      activeSection="activity-designs"
     >
       <ActivityDesignDetailContent activityDesign={activityDesign} />
     </WorkspaceShell>
